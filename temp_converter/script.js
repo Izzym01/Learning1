ed37730f861c973de774a1f3d20596c20bdf28ab
Izzym01 <76012810+Izzym01@users.noreply.github.com>
2024-03-06 10:57:13 +0000

    added toFixed string method for output

--- temp_converter/script.js
+++ temp_converter/script.js
@@ -9,12 +9,12 @@ function convert() {
     if(toFH.checked){
         temp = Number(tempinput.value);
         temp = (temp * 9 / 5) + 32;
-        results.textContent = temp + " ℉";
+        results.textContent = temp.toFixed(1) + " ℉";
     }
     else if(toCS.checked){
         temp = Number(tempinput.value);
         temp = (temp - 32) * 5 / 9;
-        results.textContent = temp + " ℃";
+        results.textContent = temp.toFixed(1) + " ℃";
     }
     else{
         results.textContent = "Select a unit";
