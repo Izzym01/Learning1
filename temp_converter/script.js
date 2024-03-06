bb55f4ad1c8cc0847fcfd7f51fc05a604b7cee81
Izzym01 <76012810+Izzym01@users.noreply.github.com>
2024-03-06 10:46:01 +0000

    temperature conversion tool

---
+++ temp_converter/script.js
@@ -0,0 +1,23 @@
+const tempinput = document.getElementById("tempinput");
+const toFH = document.getElementById("toFH");
+const toCS = document.getElementById("toCS");
+const results = document.getElementById("results");
+let temp;
+
+
+function convert() {
+    if(toFH.checked){
+        temp = Number(tempinput.value);
+        temp = (temp * 9 / 5) + 32;
+        results.textContent = temp + " ℉";
+    }
+    else if(toCS.checked){
+        temp = Number(tempinput.value);
+        temp = (temp - 32) * 5 / 9;
+        results.textContent = temp + " ℃";
+    }
+    else{
+        results.textContent = "Select a unit";
+    }
+    
+}
